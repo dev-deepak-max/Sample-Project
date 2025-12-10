fefd95710d794c4c6264be232aeb455647bafcc7
Savarapu Deepak <savarapu.deepak@delaplex.com>
2025-12-10 15:36:05 +0530

    Landing Page is on Dev

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Welcome to Simple React App</h1>
+      <p>This is a simple landing page</p>
+
+      <div className="links">
+        <a href="#">Home</a>
+        <a href="#">About Us</a>
+        <a href="#">Contact</a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
